12640a8ead4cb889be9889d9ba620eaf294b3ec3
Anshu Mishra <anshumishra@Anshus-MacBook-Air.local>
2024-07-21 19:26:07 +0530

    about us basic added

--- deepwater/urls.py
+++ deepwater/urls.py
@@ -18,6 +18,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
+from .views import AboutUSTemplate
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +27,5 @@ urlpatterns = [
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('accounts/', include('allauth.urls')),
     path('exam/', include('exam.urls')),
+    path('about-us/', AboutUSTemplate.as_view(),name="about-us"),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ deepwater/views.py
@@ -0,0 +1,4 @@
+from django.views.generic import TemplateView
+
+class AboutUSTemplate(TemplateView):
+    template_name = "about-us.html"
